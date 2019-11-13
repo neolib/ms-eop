26e236ef4f4a700b4ab5f8e7c743ffa7e2a9b2a1
Edward Yang <v-chunly@microsoft.com>
2019-11-12 23:59:53 -0800

    Renamed FindIPRange to FindIPTags

--- EOPWork/EOPWork/FindIPTags.cs
+++ EOPWork/EOPWork/FindIPTags.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Xml.Linq;
 using static System.Console;
@@ -12,6 +13,8 @@ namespace EOPWork
 {
     class FindIPTags : IApplet
     {
+        Regex ipv4Regex = new Regex(@"^\d+\.\d+\.\d+\.\d+\/\d+");
+        Regex ipv6Regex = new Regex(@"^(([\da-z]+)?:){7}([\da-z]+)?");
         List<string> tagNames = new List<string>();
 
         public int Run(string[] args)
@@ -23,6 +26,11 @@ namespace EOPWork
             tagNames.Sort();
             tagNames.ForEach((name_) => WriteLine($"{name_}"));
             return 0;
+        }
+
+        void AddTagName(string name)
+        {
+            if (!tagNames.Contains(name)) tagNames.Add(name);
         }
 
         void Process(string dir)
@@ -42,38 +50,59 @@ namespace EOPWork
 
             void WalkNode_(XElement node)
             {
-                SearchIPRange_(node);
+                var list = SearchIPRange_(node);
+                if (list.Count > 0)
+                {
+                    WriteLine($"<{node.Name}");
+                    foreach (var attr in list)
+                        WriteLine($"    {attr.Name}=\"{attr.Value}\"");
+                    WriteLine(">");
+                }
                 foreach (var child in node.Nodes())
                 {
-                    if (child is XElement elem)
-                        WalkNode_(elem);
+                    if (child is XElement elem) WalkNode_(elem);
                 }
             }
 
-            void SearchIPRange_(XElement element)
+            List<XAttribute> SearchIPRange_(XElement element)
             {
+                var list = new List<XAttribute>();
                 foreach (var attr in element.Attributes())
                 {
-                    if (ValidateAttr_(attr))
+                    if (ValidateName_(attr))
                     {
-                        if (!tagNames.Contains(attr.Name.LocalName))
-                            tagNames.Add(attr.Name.LocalName);
-                        WriteLine($"<{element.Name} {attr.Name.LocalName}=\"{attr.Value}\"/>");
+                        if (!attr.Value.StartsWith("0.0.0.0") &&
+                            attr.Value != "ffff:ffff:ffff:ffff:ffff:ffff:ffff:ffff"
+                            )
+                        {
+                            list.Add(attr);
+                            AddTagName(attr.Name.LocalName);
+                        }
                     }
                 }
+                return list;
             }
 
-            bool ValidateAttr_(XAttribute attr)
+            bool ValidateName_(XAttribute attr)
             {
                 var name = attr.Name.LocalName;
-                if (name.Contains("_IPV4") || name.Contains("IPV6") || name.EndsWith("_ip"))
+                if (name.Contains("_IPV4") || name.Contains("_IPV6") || 
+                    name.EndsWith("_IP") || name.Contains("_IP_"))
                 {
                     if (!attr.Value.Contains("-") && 
-                        (attr.Value.Contains(".") || attr.Value.Contains(":")))
+                        (attr.Value.Contains(".") || attr.Value.Contains(":"))
+                        )
                     {
                         return true;
                     }
                 }
+                return false;
+            }
+
+            bool ValidateValue_(XAttribute attr)
+            {
+                if (ipv4Regex.IsMatch(attr.Value)) return true;
+                if (ipv6Regex.IsMatch(attr.Value)) return true;
                 return false;
             }
         }
