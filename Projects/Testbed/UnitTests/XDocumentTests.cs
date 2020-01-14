ddf96f5fc7007eccd0231c72f9fa5f51b116c657
Edward Yang <v-chunly@microsoft.com>
2020-01-14 13:23:46 +0800

    README.md

--- Projects/Testbed/UnitTests/XDocumentTests.cs
+++ Projects/Testbed/UnitTests/XDocumentTests.cs
@@ -1,10 +1,5 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.IO;
 using System.Linq;
-using System.Threading.Tasks;
 using System.Xml.Linq;
 using System.Xml.XPath;
 
